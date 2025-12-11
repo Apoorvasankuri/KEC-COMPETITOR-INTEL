441c7b8aa11a334fe65e32f3373cf10caa101e2e
Apoorvasankuri <apoorvasankuri@gmail.com>
2025-12-11 17:54:52 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- Competitor Intel App_19.py
+++ Competitor Intel App_19.py
@@ -100,8 +100,8 @@ st.markdown("""
 <style>
 .logo-container-rpg {
     position: absolute;      /* attach to viewport */
-    top: 35px;            /* visible area */
-    right: 10px;          /* a bit inset from the edge */
+    top: -50px;            /* visible area */
+    right: -10px;          /* a bit inset from the edge */
     width: 90px;
     height: auto;
     z-index: 1000;        /* above other elements */
@@ -1099,6 +1099,7 @@ if uploaded_file is not None:
 if st.session_state.raw_data is not None:
 
     st.markdown('<div class="sync-status"><span class="sync-indicator"></span>Data Synced</div>', unsafe_allow_html=True)
+
 
 
 
