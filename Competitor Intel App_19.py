69fa7e71f732156c4f7cba8d19eb7a1166dcc400
Apoorvasankuri <apoorvasankuri@gmail.com>
2025-12-11 17:54:19 +0530

    Update Competitor Intel App_19.py

--- Competitor Intel App_19.py
+++ Competitor Intel App_19.py
@@ -99,7 +99,7 @@ else:
 st.markdown("""
 <style>
 .logo-container-rpg {
-    position: fixed;      /* attach to viewport */
+    position: absolute;      /* attach to viewport */
     top: 35px;            /* visible area */
     right: 10px;          /* a bit inset from the edge */
     width: 90px;
@@ -1099,6 +1099,7 @@ if uploaded_file is not None:
 if st.session_state.raw_data is not None:
 
     st.markdown('<div class="sync-status"><span class="sync-indicator"></span>Data Synced</div>', unsafe_allow_html=True)
+
 
 
 
